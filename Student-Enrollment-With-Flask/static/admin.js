521739e8a7484cb273a29c6e6cff040d39a307c6
Claire Hallock <claireehallock@gmail.com>
2024-04-04 15:16:45 -0700

    Admin page Update
    Updated admin pages to show user data.

--- Student-Enrollment-With-Flask/static/admin.js
+++ Student-Enrollment-With-Flask/static/admin.js
@@ -6,16 +6,15 @@ Create a json as a child node of the element with ID "elementID" from the json s
 No output values, it just directly edits the node in the document
 */
 function jsonToTable(elementID, json){
-    console.log(elementID);
     if(typeof json === 'undefined'){
         console.log("undefined JSON")
     }
     else{
-        const dict = JSON.parse(json); //Get the json from the json string
-        console.log(dict);
+        const dict = json;
         let table = document.createElement("table");
 
         //Headers
+        let headers = Object.keys(dict[0]);
         let headerRow = document.createElement("tr");
         for(let i = 0; i < headers.length; i++){
             let header = document.createElement("th");
@@ -26,54 +25,22 @@ function jsonToTable(elementID, json){
         }
         table.appendChild(headerRow);
 
-        console.log(dict)
         //Rows
-        let keys;
-        let grades;
-        let arrayTrue = false
-        if(dict.length > 1){
-            keys = extractNames(dict)
-            grades = extractGrades(dict)
-            arrayTrue = true
-        }
-        else{
-            keys = dict.name
-            grades = dict.grade
-        }
-        console.log(keys)
-        console.log(grades)
-        for(let i = 0; i < keys.length; i++){
+        for(let i = 0; i < dict.length; i++){
             let row = document.createElement("tr");
-
-            let key = document.createElement("td");
-            let text;
-            if(arrayTrue)
-                text = document.createTextNode(keys[i]);
-            else
-                text = document.createTextNode(keys);
-            key.appendChild(text); //Add the text to the td
+            //Add row distinction for colors
             if (i%2 ==1){
-                key.setAttribute("class", "oddRow");
+                row.setAttribute("class", "oddRow");
             }
             else{
-                key.setAttribute("class", "evenRow");
+                row.setAttribute("class", "evenRow");
             }
-            row.appendChild(key); //Add the td to the table
-
-            let value = document.createElement("td");
-            if(arrayTrue)
-                text = document.createTextNode(grades[i]);
-            else
-            text = document.createTextNode(grades);
-            value.appendChild(text);//Add the text to the td
-            if (i%2 ==1){
-                value.setAttribute("class", "oddRow");
+            for(let j in headers){
+                let key = document.createElement("td");
+                let text = document.createTextNode(dict[i][headers[j]]);
+                key.appendChild(text); //Add the text to the td
+                row.appendChild(key); //Add the td to the table
             }
-            else{
-                value.setAttribute("class", "evenRow");
-            }
-            row.appendChild(value);//Add the td to the table
-
             table.appendChild(row);
         }
         document.getElementById(elementID).appendChild(table);
@@ -81,33 +48,14 @@ function jsonToTable(elementID, json){
 }
 
 /*
-Uses the "/view_users" route to get all information in the database
-Adds a table as a child node of the element with id elementID
-Outputs the json
-
-    NOTE: Currently does not function completely properly as "/view_users" is WIP
+Adds the data to the user table
 */
-function showAllData(elementID){
-    let data;
-    thisURL = "/admin";
-    xhttp.open("GET", thisURL, async);
-    xhttp.send();
-    xhttp.onreadystatechange = () => {
-        if (xhttp.readyState === 4) {
-            if (xhttp.status == 200) {
-                data = xhttp.responseText;
-                // document.getElementById(elementID).innerHTML = ''; //Empties response
-                console.log(data);
-                jsonToTable(elementID, data);
-            }
-            else{
-                pass;
-            }
-        }
-    };
-    return(data);
+function showUserData(elementID){
+    document.getElementById(elementID).innerHTML = "Users";
+    jsonToTable(elementID, usersdata);
 }
 
 window.onload = function() {
-    showAllData("DataTable");
+    // console.log(usersdata);
+    showUserData("UserTable");
 };
